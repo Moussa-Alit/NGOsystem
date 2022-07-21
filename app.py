d730ab870179c34e99ab05205bc7d6dcdd6d7a1c
yaame-hiyyartne <moussaalit58@gmail.com>
2022-07-21 07:58:40 -0400

    fixing database uri

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ app = Flask(__name__)
 Bootstrap(app)
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:batata@localhost/database1'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://vmfbelplxyvepj:a2979b6807823c413e08a119955da592e94ab4f38696d568553ef3b11dbac674@ec2-54-87-179-4.compute-1.amazonaws.com:5432/deljs855e01f1t'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://vmfbelplxyvepj:a2979b6807823c413e08a119955da592e94ab4f38696d568553ef3b11dbac674@ec2-54-87-179-4.compute-1.amazonaws.com:5432/deljs855e01f1t'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = 'FW3434ff545h4RFE55$#f$t%yhtFF'
 
